5a3573428cfd5f42f7f82c614cfa7c784a1e62df
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-17 11:07:49 +0530

    ADDED: Method to read and print csv data with the help of POJO.

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -1,7 +1,41 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.model.CSVStateCensus;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class StateCensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to state analyser problem.");
+    //TAKING PATH OF CSV FILE
+    private static final String STATE_CENSUS_CSV_DATA = "/home/administrator/Desktop/JavaProjects/censusAnalyser/src/test/resources/StateCensusData.csv";
+    int count = 0;
+
+    //READING AND PRINTING DATA FROM CSV FILE
+    public int loadCensusCSVData() throws IOException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(STATE_CENSUS_CSV_DATA));
+        ) {
+            //WITH THE HELP OF POJO FILE ITERATING AND PRINTING THE  CONTENTS OF THE CSV FILE.
+            CsvToBean<CSVStateCensus> csvStateCensuses = new CsvToBeanBuilder(reader)
+                    .withType(CSVStateCensus.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            Iterator<CSVStateCensus> csvStateCensusIterator = csvStateCensuses.iterator();
+
+            while (csvStateCensusIterator.hasNext()) {
+                CSVStateCensus csvStateCensus = csvStateCensusIterator.next();
+                System.out.println("State: " + csvStateCensus.getState());
+                System.out.println("Area: " + csvStateCensus.getAreaInSqKm());
+                System.out.println("Density: " + csvStateCensus.getDensityPerSqKm());
+                System.out.println("Population: " + csvStateCensus.getPopulation());
+                count++;
+            }
+        }
+        return count;
     }
 }
