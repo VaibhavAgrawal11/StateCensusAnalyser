075c24d01aa385429b54d030dc3730ed788b50ec
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-17 12:27:45 +0530

    ADDED: Sad test case to verify if the exception is thrown.

--- src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
+++ src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
@@ -1,16 +1,29 @@
 package com.bridgelabz.servicetest;
 
+import com.bridgelabz.exception.StateCensusAnalyserException;
 import com.bridgelabz.service.StateCensusAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
-
 public class TestStateCensusAnalyser {
+    String STATE_CENSUS_CSV_DATA;
+    StateCensusAnalyser censusAnalyser;
     @Test
-    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws IOException {
-        StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-        int count = censusAnalyser.loadCensusCSVData();
+    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws  StateCensusAnalyserException {
+        STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.csv";
+        censusAnalyser = new StateCensusAnalyser();
+        int count = censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
         Assert.assertEquals(29, count);
+    }
+
+    @Test
+    public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() {
+        STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.csv";
+        censusAnalyser = new StateCensusAnalyser();
+        try {
+            censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FALIED,e.type);
+        }
     }
 }
