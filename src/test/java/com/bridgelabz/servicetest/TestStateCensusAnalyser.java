fa2df392dfc756916613c903e7024e91b90dc275
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-17 12:53:36 +0530

    ADDED: Sad test case to verify incorrect file type.

--- src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
+++ src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
@@ -6,24 +6,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestStateCensusAnalyser {
-    String STATE_CENSUS_CSV_DATA;
-    StateCensusAnalyser censusAnalyser;
+    String STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.csv";
+    StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+
     @Test
     public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws  StateCensusAnalyserException {
-        STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.csv";
-        censusAnalyser = new StateCensusAnalyser();
         int count = censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
         Assert.assertEquals(29, count);
     }
 
     @Test
     public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() {
-        STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.csv";
-        censusAnalyser = new StateCensusAnalyser();
         try {
             censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
         } catch (StateCensusAnalyserException e) {
-            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FALIED,e.type);
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FAILED,e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException_2() {
+        STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.pdf";
+        try {
+            censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FAILED,e.type);
         }
     }
 }
