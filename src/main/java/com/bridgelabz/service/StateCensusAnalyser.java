c19b0d4b910b64635d6beadb05a8a6389713f646
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-17 12:30:44 +0530

    MODIFIED: Thrown custum exception.

--- src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -1,9 +1,9 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.exception.StateCensusAnalyserException;
 import com.bridgelabz.model.CSVStateCensus;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -11,22 +11,17 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class StateCensusAnalyser {
-    //TAKING PATH OF CSV FILE
-    private static final String STATE_CENSUS_CSV_DATA = "/home/administrator/Desktop/JavaProjects/censusAnalyser/src/test/resources/StateCensusData.csv";
     int count = 0;
-
     //READING AND PRINTING DATA FROM CSV FILE
-    public int loadCensusCSVData() throws IOException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(STATE_CENSUS_CSV_DATA));
+    public int loadCensusCSVData(String getPath) throws StateCensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(getPath));
         ) {
             //WITH THE HELP OF POJO FILE ITERATING AND PRINTING THE  CONTENTS OF THE CSV FILE.
             CsvToBean<CSVStateCensus> csvStateCensuses = new CsvToBeanBuilder(reader)
                     .withType(CSVStateCensus.class)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
-
             Iterator<CSVStateCensus> csvStateCensusIterator = csvStateCensuses.iterator();
-
             while (csvStateCensusIterator.hasNext()) {
                 CSVStateCensus csvStateCensus = csvStateCensusIterator.next();
                 System.out.println("State: " + csvStateCensus.getState());
@@ -35,6 +30,8 @@ public class StateCensusAnalyser {
                 System.out.println("Population: " + csvStateCensus.getPopulation());
                 count++;
             }
+        } catch (IOException e) {
+            throw new StateCensusAnalyserException(e.getMessage(),StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FALIED);
         }
         return count;
     }
