1a5f7869f07581373566077f07e660faef219528
VaibhavAgrawal11 <vaihavagrawal1110@gmail.com>
2020-03-18 17:36:47 +0530

    ADDED: Test cases to check wrong delimiter

--- src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
+++ src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import java.io.File;
 
 public class TestStateCensusAnalyser {
-    String STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.csv";
+    String STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.csv";
     StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
 
     @Test
@@ -20,6 +20,7 @@ public class TestStateCensusAnalyser {
     @Test
     public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() {
         try {
+            STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.csv";
             censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FAILED, e.type);
@@ -36,4 +37,16 @@ public class TestStateCensusAnalyser {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.WRONG_FILE_TYPE, e.type);
         }
     }
+
+    @Test
+    public void givenStateCensusAnalyserFile_WhenIncorrectDelimiter_ReturnCustomException() {
+        try {
+            STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusDataCopy.csv";
+            File delimiterCheck = new File(STATE_CENSUS_CSV_DATA);
+            censusAnalyser.checkDelimiter(delimiterCheck);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INVALID_DELIMITER, e.type);
+        }
+    }
+
 }
