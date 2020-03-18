cbac029c04efc0e78d7d86569c6b44b243c6be17
VaibhavAgrawal11 <vaihavagrawal1110@gmail.com>
2020-03-18 12:48:41 +0530

    ADDED: Sad test case to check file extension.

--- src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
+++ src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
@@ -5,25 +5,35 @@ import com.bridgelabz.service.StateCensusAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+
 public class TestStateCensusAnalyser {
-    String STATE_CENSUS_CSV_DATA;
-    StateCensusAnalyser censusAnalyser;
+    String STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.csv";
+    StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+
     @Test
-    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws  StateCensusAnalyserException {
-        STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.csv";
-        censusAnalyser = new StateCensusAnalyser();
+    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws StateCensusAnalyserException {
         int count = censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
         Assert.assertEquals(29, count);
     }
 
     @Test
     public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() {
-        STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.csv";
-        censusAnalyser = new StateCensusAnalyser();
         try {
             censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
         } catch (StateCensusAnalyserException e) {
-            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FALIED,e.type);
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FALIED, e.type);
+        }
+    }
+
+    @Test
+    public void givenStateCensusAnalyserFile_WhenIncorrectType_ReturnCustomException() {
+        STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.pdf";
+        try {
+            File fileExtension = new File(STATE_CENSUS_CSV_DATA);
+            censusAnalyser.getFileExtension(fileExtension);
+        } catch (StateCensusAnalyserException e) {
+            Assert.assertEquals(StateCensusAnalyserException.ExceptionType.WRONG_FILE_TYPE, e.type);
         }
     }
 }
