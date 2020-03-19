d6650eeb98a73db8eab7d3104608f95951045761
VaibhavAgrawal11 <vaihavagrawal1110@gmail.com>
2020-03-19 12:03:30 +0530

    ADDED: Test case to check count of the records in the csv file.

--- src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
+++ src/test/java/com/bridgelabz/servicetest/TestStateCensusAnalyser.java
@@ -8,20 +8,22 @@ import org.junit.Test;
 import java.io.File;
 
 public class TestStateCensusAnalyser {
-    String STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusData.csv";
+
+    String stateCensusCsvData = "./src/test/resources/StateCensusData.csv";
+    String stateCodeCsvData = "./src/test/resources/StateCode.csv";
     StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
 
     @Test
-    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws  StateCensusAnalyserException {
-        int count = censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
+    public void givenStateCensusAnalyserFile_WhenTrue_NumberOfRecordShouldMatch() throws StateCensusAnalyserException {
+        int count = censusAnalyser.loadCensusCSVData(stateCensusCsvData);
         Assert.assertEquals(29, count);
     }
 
     @Test
     public void givenStateCensusAnalyserFile_WhenImproperFileName_ReturnsException() {
         try {
-            STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.csv";
-            censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
+            stateCensusCsvData = "./src/test/resources/EstateCensusData.csv";
+            censusAnalyser.loadCensusCSVData(stateCensusCsvData);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INPUT_OUTPUT_OPERATION_FAILED, e.type);
         }
@@ -29,9 +31,9 @@ public class TestStateCensusAnalyser {
 
     @Test
     public void givenStateCensusAnalyserFile_WhenIncorrectType_ReturnCustomException() {
-        STATE_CENSUS_CSV_DATA = "./src/test/resources/EstateCensusData.pdf";
+        stateCensusCsvData = "./src/test/resources/EstateCensusData.pdf";
         try {
-            File fileExtension = new File(STATE_CENSUS_CSV_DATA);
+            File fileExtension = new File(stateCensusCsvData);
             censusAnalyser.getFileExtension(fileExtension);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.WRONG_FILE_TYPE, e.type);
@@ -41,8 +43,8 @@ public class TestStateCensusAnalyser {
     @Test
     public void givenStateCensusAnalyserFile_WhenIncorrectDelimiter_ReturnCustomException() {
         try {
-            STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusDataCopy.csv";
-            File delimiterCheck = new File(STATE_CENSUS_CSV_DATA);
+            stateCensusCsvData = "./src/test/resources/StateCensusDataCopy.csv";
+            File delimiterCheck = new File(stateCensusCsvData);
             censusAnalyser.checkDelimiter(delimiterCheck);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.ExceptionType.INVALID_DELIMITER, e.type);
@@ -52,11 +54,16 @@ public class TestStateCensusAnalyser {
     @Test
     public void givenStateCensusAnalyserFile_WhenIncorrectHeader_ReturnCustomException() {
         try {
-            STATE_CENSUS_CSV_DATA = "./src/test/resources/StateCensusDataHeaderCopy.csv";
-            censusAnalyser.loadCensusCSVData(STATE_CENSUS_CSV_DATA);
+            stateCensusCsvData = "./src/test/resources/StateCensusDataHeaderCopy.csv";
+            censusAnalyser.loadCensusCSVData(stateCensusCsvData);
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals("Number of data fields does not match number of headers.", e.getMessage());
         }
     }
 
+    @Test
+    public void givenStateCodeCsvFile_WhenTrue_NumberOfRecordShouldMatch() throws StateCensusAnalyserException {
+        int count = censusAnalyser.loadStateCodeData(stateCodeCsvData);
+        Assert.assertEquals(37, count);
+    }
 }
