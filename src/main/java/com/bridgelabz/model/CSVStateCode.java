432c4b9b21888436b3fbb7f3d5d4ee170be0f11a
VaibhavAgrawal11 <vaihavagrawal1110@gmail.com>
2020-03-19 12:02:36 +0530

    ADDED: POJO class to load all data from StateCode.csv file.

---
+++ src/main/java/com/bridgelabz/model/CSVStateCode.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.model;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCode {
+
+    @CsvBindByName(column = "SrNo")
+    private String SrNo;
+
+    @CsvBindByName(column = "State")
+    private String StateName;
+
+    @CsvBindByName(column = "TIN")
+    private String TIN;
+
+    @CsvBindByName(column = "StateCode")
+    private String StateCode;
+
+    public String getSrNo() {
+        return SrNo;
+    }
+
+    public void setSrNo(String srNo) {
+        SrNo = srNo;
+    }
+
+    public String getStateName() {
+        return StateName;
+    }
+
+    public void setStateName(String stateName) {
+        StateName = stateName;
+    }
+
+    public String getTIN() {
+        return TIN;
+    }
+
+    public void setTIN(String TIN) {
+        this.TIN = TIN;
+    }
+
+    public String getStateCode() {
+        return StateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        StateCode = stateCode;
+    }
+}
